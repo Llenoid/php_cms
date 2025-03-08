1720634415882969ba6eeb16e5fa583e0a3b1e45
brandon <brandon1236perez@gmail.com>
2025-03-08 21:28:29 +0800

    feat: Added css and js for navbar styling and functionality

---
+++ includes/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">CMS</a>
+    <button
+      data-mdb-collapse-init
+      class="navbar-toggler"
+      type="button"
+      data-mdb-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Dashboard</a>
+        <a class="nav-link" href="#">Logout</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include('includes/config.php');
+include('includes/database.php');
+include('includes/functions.php');
+include('includes/header.php');
+
+include('includes/navbar.php');
+
+include('includes/footer.php');
+?>
